4a2d10003241394b37ae5c27f41172f04c49a323
ddiu8081 <ddiu8081@gmail.com>
2023-09-01 04:17:43 +0800

    feat: index ui

--- src/components/WelcomeBox.tsx
+++ src/components/WelcomeBox.tsx
@@ -1,7 +1,40 @@
+import { createSignal } from 'solid-js'
+import {
+  Segment,
+  SegmentControl,
+  SegmentGroup,
+  SegmentIndicator,
+  SegmentLabel,
+} from '@ark-ui/solid'
+import clsx from 'clsx'
+import { Presentation, MousePointer2 } from 'lucide-solid'
+
 export default () => {
+  const [value, setValue] = createSignal('presenter')
   return (
-    <div class="w-full max-w-xl mx-auto bg-base-100 p-6 rounded-lg border border-base">
-      111
+    <div class="flex w-full gap-4 bg-base p-6 rounded-xl border border-base shadow">
+      <button
+        class={clsx([
+          'flex-1 flex-col gap-2 fcc rounded-md p-4 font-medium',
+          'border-2 border-base shadow-sm',
+          'bg-transparent hover:bg-base-100 hover:fg-emphasis transition-colors',
+          'focus-visible:outline-none focus-visible:ring-1',
+        ])}
+      >
+        <Presentation size={28} />
+        <div>屏幕</div>
+      </button>
+      <button
+        class={clsx([
+          'flex-1 flex-col gap-2 fcc rounded-md p-4 font-medium',
+          'border-2 border-base shadow-sm',
+          'bg-transparent hover:bg-base-100 hover:fg-emphasis transition-colors',
+          'focus-visible:outline-none focus-visible:ring-1',
+        ])}
+      >
+        <MousePointer2 size={28} />
+        <div>控制器</div>
+      </button>
     </div>
   )
 }
