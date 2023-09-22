97b8e62df21cc3e567a639c19c6d6baff405d342
ddiu8081 <ddiu8081@gmail.com>
2023-09-22 18:19:42 +0800

    fix(ui): remove text margin in song-cover view

--- src/components/screen/SongCoverScreenView.tsx
+++ src/components/screen/SongCoverScreenView.tsx
@@ -9,7 +9,7 @@ export default (props: Props) => {
   return (
     <div>
       <Show when={props.detail.meta?.year}>
-        <p class="text-[120px] text-[16vmin] leading-tight mx-4">{props.detail.meta.year}</p>
+        <p class="text-[120px] text-[16vmin] leading-tight">{props.detail.meta.year}</p>
       </Show>
       <p class="text-[220px] text-[28vmin] leading-tight font-bold">{props.detail.title}</p>
     </div>
