161c60e0788e70065d044bc5c730fe149cea082d
ddiu8081 <ddiu8081@gmail.com>
2023-10-12 23:36:49 +0800

    fix: hide full-screen control area when no song

--- src/components/screen/FloatControlBar.tsx
+++ src/components/screen/FloatControlBar.tsx
@@ -1,4 +1,4 @@
-import { Show } from 'solid-js'
+import { Switch, Match } from 'solid-js'
 import { useStore } from '@nanostores/solid'
 import { X, ChevronLeft, ChevronRight } from 'lucide-solid'
 import { $extraView } from '@/stores/coreState'
@@ -13,19 +13,21 @@ export default () => {
 
   return (
     <div class="flex gap-2">
-      <Show when={extraView()}>
-        <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'set_extra', payload: null })}>
-          <X class="op-25" />
-        </Button>
-      </Show>
-      <Show when={currentSongData()}>
-        <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'prev' })}>
-          <ChevronLeft class="op-25" />
-        </Button>
-        <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'next' })}>
-          <ChevronRight class="op-25" />
-        </Button>
-      </Show>
+      <Switch>
+        <Match when={extraView()}>
+          <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'set_extra', payload: null })}>
+            <X class="op-25" />
+          </Button>
+        </Match>
+        <Match when={currentSongData()}>
+          <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'prev' })}>
+            <ChevronLeft class="op-25" />
+          </Button>
+          <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'next' })}>
+            <ChevronRight class="op-25" />
+          </Button>
+        </Match>
+      </Switch>
       <FloatControlBarMenuButton />
     </div>
   )
--- src/components/screen/FullScreenControlArea.tsx
+++ src/components/screen/FullScreenControlArea.tsx
@@ -1,8 +1,13 @@
+import { Show } from 'solid-js'
+import { useStore } from '@nanostores/solid'
 import { $coreState } from '@/composables'
+import { $currentSongData } from '@/stores/data'
 
 export default () => {
+  const currentSongData = useStore($currentSongData)
+
   return (
-    <>
+    <Show when={currentSongData()}>
       <div
         class="absolute left-0 w-30vw top-20 bottom-20 z-10"
         onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'prev' })}
@@ -11,6 +16,6 @@ export default () => {
         class="absolute right-0 w-30vw top-20 bottom-20 z-10"
         onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'next' })}
       />
-    </>
+    </Show>
   )
 }
