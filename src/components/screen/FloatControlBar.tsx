e08b46813f980ab16419cd76d07352d041e3e2ca
ddiu8081 <ddiu8081@gmail.com>
2023-10-12 22:26:50 +0800

    feat: float btns

--- src/components/screen/FloatControlBar.tsx
+++ src/components/screen/FloatControlBar.tsx
@@ -1,17 +1,31 @@
-import { ChevronLeft, ChevronRight } from 'lucide-solid'
+import { Show } from 'solid-js'
+import { useStore } from '@nanostores/solid'
+import { X, ChevronLeft, ChevronRight } from 'lucide-solid'
+import { $extraView } from '@/stores/coreState'
+import { $currentSongData } from '@/stores/data'
 import { $coreState } from '@/composables'
 import Button from '@/components/common/Button'
 import FloatControlBarMenuButton from './FloatControlBarMenuButton'
 
 export default () => {
+  const extraView = useStore($extraView)
+  const currentSongData = useStore($currentSongData)
+
   return (
     <div class="flex gap-2">
-      <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'prev' })}>
-        <ChevronLeft class="op-25" />
-      </Button>
-      <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'next' })}>
-        <ChevronRight class="op-25" />
-      </Button>
+      <Show when={extraView()}>
+        <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'set_extra', payload: null })}>
+          <X class="op-25" />
+        </Button>
+      </Show>
+      <Show when={currentSongData()}>
+        <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'prev' })}>
+          <ChevronLeft class="op-25" />
+        </Button>
+        <Button size="large" variant="outline" onClick={() => $coreState.triggerAction({ type: 'show_prev_next_line', payload: 'next' })}>
+          <ChevronRight class="op-25" />
+        </Button>
+      </Show>
       <FloatControlBarMenuButton />
     </div>
   )
