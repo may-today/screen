480d91ed961dc7fa66ebf0313422dfda9d8d547d
ddiu8081 <ddiu8081@gmail.com>
2024-05-04 14:55:10 +0800

    fix: adjust page padding

--- src/components/screen/Page.tsx
+++ src/components/screen/Page.tsx
@@ -15,7 +15,7 @@ export default () => {
   const blackScreen = useStore($blackScreen)
 
   return (
-    <div class="w-screen h-[-webkit-fill-available] h-[100svh] p-[4vmin] text-[30vmin] overflow-hidden">
+    <div class="w-screen h-[-webkit-fill-available] h-[100svh] p-[2vmin] text-[30vmin] overflow-hidden">
       <Presence exitBeforeEnter>
         <Switch>
           <Match when={blackScreen()}>
